727fabe45f4d827b5d961458321094e51f7c253a
David-Flores-Ruiz <39530033+David-Flores-Ruiz@users.noreply.github.com>
2018-06-30 21:06:18 -0500

    Tarea4.- Parte2   ----InfixToPostfix----

---
+++ Tarea4.- Parte2_InfixToPostfix/src/Stack.h
@@ -0,0 +1,32 @@
+/*
+ * Stack.h
+ *
+ *  Created on: 18/06/2018
+ *      Author: administrador
+ */
+
+#ifndef STACK_H_
+#define STACK_H_
+
+typedef enum {false, true} bool;
+
+typedef void * Type;
+
+typedef struct strStack *Stack;
+
+Stack createStack();
+
+void destroyStack(Stack);
+
+bool push(Stack, Type);
+
+Type pop(Stack);
+
+Type top(Stack);
+
+bool isEmpty(Stack);
+
+#endif /* STACK_H_ */
+
+
+
